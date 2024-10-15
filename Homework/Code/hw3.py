ec18039993c459aaf79aa7b0e4ea079fda58023e
Zoë Langhoff-Weinstein <zlweinstein@wisc.edu>
2024-10-15 14:48:44 -0700

    P3
    Adding P3 Python file and homework description

---
+++ Homework/Code/hw3.py
@@ -0,0 +1,30 @@
+from scipy.linalg import eigh
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_and_center_dataset(filename):
+    # Your implementation goes here!
+    raise NotImplementedError
+
+def get_covariance(dataset):
+    # Your implementation goes here!
+    raise NotImplementedError
+
+def get_eig(S, m):
+    # Your implementation goes here!
+    raise NotImplementedError
+
+def get_eig_prop(S, prop):
+    # Your implementation goes here!
+    raise NotImplementedError
+
+def project_image(image, U):
+    # Your implementation goes here!
+    raise NotImplementedError
+
+def display_image(orig, proj):
+    # Your implementation goes here!
+    # Please use the format below to ensure grading consistency
+    # fig, ax1, ax2 = plt.subplots(figsize=(9,3), ncols=2)
+    # return fig, ax1, ax2
+    raise NotImplementedError
